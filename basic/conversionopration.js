5552f0976c569a9355ea97c38c4f7007a62f83c5
sunny chadotara <chadotara.sunny27@gmail.com>
2024-12-31 14:18:43 +0530

    new file

--- basic/conversionopration.js
+++ basic/conversionopration.js
@@ -5,10 +5,14 @@ console.log(typeof score);
 console.log(typeof "score");
 
 let valueInNumber  = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-let 
+// ================================ Oprations ========================================== 
+
+
+
+
 
 
 
