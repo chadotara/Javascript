1a69b622a1701af96e0ed939b67514ab47db52f5
sunny chadotara <chadotara.sunny27@gmail.com>
2025-01-19 12:26:23 +0530

    pro6

---
+++ pro6/script.js
@@ -0,0 +1,46 @@
+const notesContainer = document.querySelector(".notes");
+const createBtn = document.querySelector(".btn");
+
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+showNotes();
+
+function updateStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+createBtn.addEventListener("click", () => {
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img"); // Create the icon element
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "/pro3/images/unchecked.png";
+
+  // Add the appropriate classes to the icon
+  notesContainer.appendChild(inputBox).appendChild(img); // Append the span to the inputBox
+});
+
+notesContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+    updateStorage();
+  } else if (e.target.tagName === "P") {
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach((nt) => {
+      nt.onkeyup = function () {
+        updateStorage();
+      };
+    });
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    document.execCommand("insertLineBreak");
+    event.preventDefault();
+  }
+});
