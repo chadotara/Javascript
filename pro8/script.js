1186dae70b26d34a3d7425120e650cac3b658757
sunny chadotara <chadotara.sunny27@gmail.com>
2025-01-19 16:33:33 +0530

    pro8

---
+++ pro8/script.js
@@ -0,0 +1,21 @@
+const api_url = "https:///api.quotable.io/random";
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
+async function getQuote(url) {
+  const response = await fetch(url);
+  var data = await response.json();
+  quote.innerHTML = data.content;
+  author.innerHTML = data.author;
+}
+
+getQuote(api_url);
+
+function tweet() {
+  window.open(
+    "https://twitter.com/intent/tweet?text= " + quote.innerHTML + "---- by " + author.innerHTML,
+    "Tweet window",
+    "width=600",
+    "hight=300"
+  );
+}
